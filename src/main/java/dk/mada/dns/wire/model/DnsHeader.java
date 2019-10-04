b7b488adc0627c1d5ed1a53a6922ea4deb2990dc
Jesper Skov <jskov@zoftcorp.dk>
2019-10-04 19:37:54 +0200

    fix qr flag

--- src/main/java/dk/mada/dns/wire/model/DnsHeader.java
+++ src/main/java/dk/mada/dns/wire/model/DnsHeader.java
@@ -2,12 +2,24 @@ package dk.mada.dns.wire.model;
 
 import java.nio.ByteBuffer;
 
+import dk.mada.dns.util.Hexer;
+
 /**
  * DNS message header.
  * 
  * From https://www.ietf.org/rfc/rfc1035.txt, 4.1.1. Header section format
  */
 public abstract class DnsHeader {
+	// Header flags overview https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+	public static final short FLAGS_QR = (short)0x8000; // response when set
+	public static final int FLAGS_AA = 0x0400; // Authoritative Answer
+	public static final int FLAGS_TC = 0x0200; // TrunCation
+	public static final int FLAGS_RD = 0x0100; // Recursion Desired
+	public static final int FLAGS_RA = 0x0080; // Recursion Available
+	public static final int FLAGS_Z =  0x0040; // Reserved
+	public static final int FLAGS_AD = 0x0020; // Authentic data https://tools.ietf.org/html/rfc6895, https://tools.ietf.org/html/rfc6840#section-5.7
+	public static final int FLAGS_CD = 0x0010; // Checking Disabled
+
 	private final short id;
 	private final short flags;
 	private final short qdcount;
@@ -72,4 +84,42 @@ public abstract class DnsHeader {
 		return bb.array();
 	}
 
+	public String toDebugString() {
+		String qr = ((flags & 0x8000) == 0) ? "Q" : "R";
+		int rcode = flags & 0x000f;
+		String status = rcode == 0 ? "OK" : ("ERR(" + String.format("0x%01x", rcode) + ")");
+		System.out.println("GOT " + ((flags >> 8) & 0x0001));
+		
+		String aa = ((flags & FLAGS_AA) != 0) ? "aa" : "";
+		String tc = ((flags & FLAGS_TC) != 0) ? "tc" : "";
+		String rd = ((flags & FLAGS_RD) != 0) ? "rd" : "";
+		String ra = ((flags & FLAGS_RA) != 0) ? "ra" : "";
+		String ad = ((flags & FLAGS_AD) != 0) ? "ad" : "";
+		String cd = ((flags & FLAGS_CD) != 0) ? "cd" : "";
+		int opcode = ((flags >> 11) & 0x000f);
+		String opc = "?";
+		if (opcode == 0) {
+			opc = "query";
+		} else if (opcode == 1) {
+			opc = "iquery";
+		} else if (opcode == 2) {
+			opc = "status";
+		}
+		
+		return new StringBuilder()
+				.append("id:").append(Hexer.hexShort(id))
+				.append(", flags:").append(Hexer.hexShort(flags))
+				.append(" (")
+				.append(qr).append(",")
+				.append(opc).append(",")
+				.append(aa).append(",")
+				.append(tc).append(",")
+				.append(rd).append(",")
+				.append(ra).append(",")
+				.append(ad).append(",")
+				.append(cd).append(",")
+				.append(status)
+				.append(")")
+				.toString();
+	}
 }
