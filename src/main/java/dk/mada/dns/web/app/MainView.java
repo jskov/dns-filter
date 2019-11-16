ee0045fc1eb29dbe9d4f5aa897b8a79a727ff253
Jesper Skov <jskov@zoftcorp.dk>
2019-11-16 11:40:38 +0100

    can add/remove whitelisted hosts

--- src/main/java/dk/mada/dns/web/app/MainView.java
+++ src/main/java/dk/mada/dns/web/app/MainView.java
@@ -11,11 +11,12 @@ import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.vaadin.flow.component.AbstractField.ComponentValueChangeEvent;
+import com.vaadin.flow.component.HasValue.ValueChangeListener;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Label;
-import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
@@ -34,6 +35,7 @@ import dk.mada.dns.config.Configuration;
 public class MainView extends VerticalLayout {
 	private static final Logger logger = LoggerFactory.getLogger(MainView.class);
 	@Inject	private Configuration config;
+	private ListBox<String> whitelistList;
 
 	public MainView() {
 		
@@ -52,32 +54,65 @@ public class MainView extends VerticalLayout {
 	
 	@PostConstruct
 	public void after() {
-		List<String> whitelistedNames = config.getWhitelistedHostNames().stream()
-			.sorted()
-			.collect(toList());
 		
 		
-		ListBox<String> whitelistList = new ListBox<>();
-		whitelistList.setItems(whitelistedNames);
+		whitelistList = new ListBox<>();
+		updateWhitelistedHostNames();
+		
+		config.addChangeListener(this::updateWhitelistedHostNames);
 		
 		
 		whitelistList.setRenderer(makeRenderer());		
 		add(new H3("Whitelisted hosts"), whitelistList);
 	
+		TextField wlHost = new TextField("Whitelist host:");
+		wlHost.setClearButtonVisible(true);
 		
+		TextField wlHostReason = new TextField("Reason:");
+		wlHostReason.setClearButtonVisible(true);
+
+		Button button = new Button(new Icon(VaadinIcon.PLUS), event -> {
+			String host = wlHost.getValue();
+			String reason = wlHostReason.getValue();
+			logger.info("Add whitelist of host {} because {}", host, reason);
+			
+			wlHost.clear();
+			wlHostReason.clear();
+			config.whitelistHost(host, reason);
+		});
+		button.setEnabled(false);
 		
+		ValueChangeListener<? super ComponentValueChangeEvent<TextField, String>> onInputChange = vc -> {
+			boolean inputValid = !(wlHost.isEmpty() || wlHostReason.isEmpty());
+			button.setEnabled(inputValid);
+		};
+		
+		wlHost.addValueChangeListener(onInputChange);
+		wlHostReason.addValueChangeListener(onInputChange);
+
+		
+		Div wl = new Div(wlHost, wlHostReason, button);
+		add(wl);
 		
 		logger.info("After creation {}", config);
 	}
 
-	private ComponentRenderer<Div, String> makeRenderer() {
+	private void updateWhitelistedHostNames() {
+		logger.info("Updating list of whitelisted hosts");
+		List<String> whitelistedNames = config.getWhitelistedHostNames().stream()
+				.sorted()
+				.collect(toList());
+		whitelistList.setItems(whitelistedNames);
+	}
 
+	private ComponentRenderer<Div, String> makeRenderer() {
 		return new ComponentRenderer<>(item -> {
 		    Label name = new Label(item);
 	
 		    Button button = new Button(new Icon(VaadinIcon.TRASH), event -> {
 		    	logger.info("Delete {}", item);
-//		        whitelistListBox.getDataProvider().refreshItem(item);
+		    	config.unwhitelistHost(item);
+		    	updateWhitelistedHostNames();
 		    });
 	
 		    Div labels = new Div(name);
