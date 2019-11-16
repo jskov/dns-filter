d02140f77df37ab09fc398713609be88265ee98b
Jesper Skov <jskov@zoftcorp.dk>
2019-11-16 10:58:19 +0100

    Works with CDI

--- src/main/java/dk/mada/dns/web/app/MainView.java
+++ src/main/java/dk/mada/dns/web/app/MainView.java
@@ -1,5 +1,13 @@
 package dk.mada.dns.web.app;
 
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.notification.Notification;
@@ -8,18 +16,36 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
 
+import dk.mada.dns.config.Configuration;
+
 @Route(value = "")
 @PWA(name = "My UI in Java", shortName = "Hello world")
 public class MainView extends VerticalLayout {
-  public MainView() {
-    TextField name = new TextField("Name");
-    Paragraph greeting = new Paragraph("");
+	private static final Logger logger = LoggerFactory.getLogger(MainView.class);
+	@Inject	private Configuration config;
 
-    Button button = new Button("Click me",
-            event -> Notification.show("Jeps"));
-    add(button);
+	public MainView() {
+		
+		logger.info("At creation {}", config);
+		
+		
+		
+		TextField name = new TextField("Name");
+		Paragraph greeting = new Paragraph("");
 
+		Button button = new Button("Click me", event -> Notification.show("Jeps " + Objects.toString(config)));
+		add(button);
 
-    add(name, button, greeting);
-  }
+		add(name, button, greeting);
+	}
+	
+	@PostConstruct
+	public void after() {
+		
+		TextField x = new TextField("Help");
+		
+		add(x);
+		
+		logger.info("After creation {}", config);
+	}
 }
