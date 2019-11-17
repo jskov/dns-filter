9b26132a138dc92d7a1e0600a23bc15e63915a22
Jesper Skov <jskov@zoftcorp.dk>
2019-11-17 11:25:19 +0100

    import cleanup

--- src/main/java/dk/mada/dns/wire/model/conversion/WireToModelConverter.java
+++ src/main/java/dk/mada/dns/wire/model/conversion/WireToModelConverter.java
@@ -7,7 +7,6 @@ import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
