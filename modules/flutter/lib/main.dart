f1e5594383a68232bc7d27e3cdf9d4f535e7dbc7
Jesper Skov <jskov@zoftcorp.dk>
2020-07-23 09:58:52 +0200

    actually gets events

--- modules/flutter/lib/main.dart
+++ modules/flutter/lib/main.dart
@@ -15,6 +15,7 @@ void main() {
 class MyApp extends StatelessWidget {
   void websocket() {
     dev.log('!create channel!', name: 'dk.mada.ws');
+    /*
     var channel = IOWebSocketChannel.connect("ws://10.0.2.2:8080/chat/flutter");
 
     channel.sink.add("Connected!");
@@ -23,6 +24,7 @@ class MyApp extends StatelessWidget {
       channel.sink.add("received!");
       channel.sink.close(status.goingAway);
     });
+    */
   }
 
   // This widget is the root of your application.
@@ -34,28 +36,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.red,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutters Home Page'),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.red,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: MyHomePage(
+          title: 'Flutters Home Page',
+          channel:
+              IOWebSocketChannel.connect("ws://10.0.2.2:8080/chat/flutter")),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  final IOWebSocketChannel channel;
+  MyHomePage({Key key, this.title, @required this.channel}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -127,6 +132,15 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headline5,
             ),
+            StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, snapshot) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
           ],
         ),
       ),
