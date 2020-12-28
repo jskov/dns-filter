3fdc7281e597767e60334825343546509c98996d
Jesper Skov <jskov@zoftcorp.dk>
2020-12-28 12:38:54 +0100

    build as container

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 export VERSION=${VERSION:-workspace}
 
-podman build --no-cache -v /opt/sources/_gradle:/cache/_gradle:Z,rw -v $(pwd):/src:Z,rw,shared -f modules/container/Containerfile --build-arg VERSION=${VERSION} .
+podman build --no-cache -v /opt/sources/_gradle:/cache/_gradle:Z,rw -v $(pwd):/src:Z,rw,shared -f modules/container/Containerfile --build-arg VERSION=${VERSION} . "$@"
+
