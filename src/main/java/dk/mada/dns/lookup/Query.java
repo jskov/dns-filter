5347de9ed9c1b4e476f17053b745a79c0ea0a25a
Jesper Skov <jskov@zoftcorp.dk>
2021-07-01 12:50:19 +0200

    put in query

--- src/main/java/dk/mada/dns/lookup/Query.java
+++ src/main/java/dk/mada/dns/lookup/Query.java
@@ -1,18 +1,47 @@
 package dk.mada.dns.lookup;
 
+import java.nio.ByteBuffer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dk.mada.dns.wire.model.DnsReplies;
+import dk.mada.dns.wire.model.DnsReply;
 import dk.mada.dns.wire.model.DnsRequest;
+import dk.mada.dns.wire.model.DnsRequests;
 
 /**
  * Represents a DNS query from a client.
+ *
+ * Holds the data of a DNS query.
+ * 
+ * Elements are wire-request, model-request, model-reply, and wire-reply.
+ * These are added strictly in that order.
+ * 
+ * The collection of data makes it easier to log information about
+ * failed query processing.
+ * 
  */
 public class Query {
+	private static final Logger logger = LoggerFactory.getLogger(Query.class);
+	
 	private final String clientIp;
-	private DnsRequest request;
+	private final ByteBuffer wireRequest;
+	private final DnsRequest request;
+	private DnsReply reply;
+	private ByteBuffer wireReply;
 	private boolean debugBypassRequest;
 
-	public Query(DnsRequest request, String clientIp) {
-		this.request = request;
+	private Query(String clientIp, ByteBuffer wireRequest) {
 		this.clientIp = clientIp;
+		this.wireRequest = wireRequest.asReadOnlyBuffer();
+
+		this.request = DnsRequests.fromWireData(this.wireRequest);
+		this.wireRequest.rewind();
+	}
+
+	public static Query newFromIp(String clientIp, ByteBuffer wireRequest) {
+		return new Query(clientIp, wireRequest);
 	}
 	
 	public String getRequestName() {
@@ -40,5 +69,24 @@ public class Query {
 
 	public void setDebugBypassRequest(boolean debugBypassRequest) {
 		this.debugBypassRequest = debugBypassRequest;
+	}
+
+	public void setLookupResult(LookupResult lr) {
+		reply = lr.getReply();
+		if (lr.getState() == LookupState.BYPASS) {
+			logger.warn("Bypass lookup");
+			wireReply = reply.getOptWireReply();
+		} else {
+			logger.debug("Decoded reply: {}", reply);
+			wireReply = DnsReplies.toWireFormat(reply);
+		}
+	}
+
+	public DnsReply getReply() {
+		return reply;
+	}
+	
+	public ByteBuffer getWireReply() {
+		return wireReply.asReadOnlyBuffer();
 	}
 }
